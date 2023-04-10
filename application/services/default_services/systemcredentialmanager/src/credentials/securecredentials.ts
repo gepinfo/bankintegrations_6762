ba85b46ed1d858948b9abe8ddd206aeb7c2f1724
gepinfo <info@geppettosoftware.com>
2023-04-10 09:05:30 +0000

    code commited from geppetto!

---
+++ application/services/default_services/systemcredentialmanager/src/credentials/securecredentials.ts
@@ -0,0 +1,4 @@
+export let credentials:any = {
+    DB_AUTH_bankintegrations_6762: 'mongodb://admin:password@',
+    DB_COLL_bankintegrations_6762: ':27017/bankintegrations_6762?authSource=admin'
+}
